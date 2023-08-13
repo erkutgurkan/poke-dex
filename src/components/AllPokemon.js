58c92409511935d0e569b51ecf0dc5c29dee3300
Erkut Gürkan <111708975+sirrsolaire@users.noreply.github.com>
2023-08-13 18:13:22 +0300

    more api call

--- src/components/AllPokemon.js
+++ src/components/AllPokemon.js
@@ -19,10 +19,21 @@ function AllPokemon() {
           {sortedAndFilteredRecords.map((pokemon) => (
             <PokemonCard
               pokeImage={pokemon.sprites.front_default}
+              pokeImageBack={pokemon.sprites.back_default}
               pokeId={pokemon.id}
               pokeExp={pokemon.base_experience}
               pokeName={pokemon.name}
               pokeType={pokemon.types[0].type.name}
+              pokeAbility={pokemon.abilities[0].ability.name}
+              pokeForms={pokemon.forms[0].name}
+              pokeHeight={pokemon.height}
+              pokeWeight={pokemon.weight}
+              pokeMoves={pokemon.moves[0].move.name}
+              pokeOrder={pokemon.order}
+              pokeStatsHp={pokemon.stats[0].base_stat}
+              pokeStatsAttack={pokemon.stats[1].base_stat}
+              pokeStatsDefense={pokemon.stats[2].base_stat}
+              pokeStatsSpeed={pokemon.stats[5].base_stat}
               key={pokemon.id}
             />
           ))}
